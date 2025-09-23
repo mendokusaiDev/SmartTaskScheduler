1919cad44f288b75316c4b6cc1df1bce361c741e
azbayor2 <hanharry2@gmail.com>
2025-09-23 14:20:21 +0900

    프로그램 구조 추가 (중간 저장#)

--- Smart Task Scheduler/Calender.h
+++ Smart Task Scheduler/Calender.h
@@ -2,18 +2,53 @@
 
 namespace scheduler {
 
-	class cal_Month {
+	class cal_Year {
+		/*
+		멤버 변수: cal_Month의 headptr을 저장
 
+		멤버 함수: 년도 추가하기
+				   월 추가하기
+		*/
+
+	};
+	
+	class cal_Month {
+		/*
+		멤버 변수: cal_Day의 headptr을 저장
+
+		멤버 함수: 월 추가하기
+		           일 추가하기
+		*/
 
 	};
 
 	class cal_Day {
+		/*
+		멤버 변수: 하루 일정 (그 하루에 해당하는 Task 를 LinkedList 형식으로 저장)
 
+		멤버 함수: 일정 할당하기
+		           일정 리셋하기
+		*/
 
 	};
 
 	class Calender {
+		/*
+		멤버 변수: cal_Year의 headptr을 저장
+		           통계 저장
 
+				 
+
+		멤버 함수: 일정 추가하기
+				   일정 삭제하기
+				   일정 인터벌 바꾸기
+				   월 단위 주요 일정 정보 가져오기
+				   주 단위 일정 정보 가져오기
+				   일 단위 일정 정보 가져오기
+				   일정 변경하기
+				   통계 불러오기
+		
+		*/
 
 	};
 }
--- Smart Task Scheduler/Scheduler.h
+++ Smart Task Scheduler/Scheduler.h
@@ -3,6 +3,10 @@
 namespace scheduler {
 	
 	class scheduler {
-
+		/*
+			멤버 변수: 우선순위 큐
+			멤버 함수: 앞으로 일정 만들기
+					   스케줄러 인터벌 설정
+		*/
 	};
 }
