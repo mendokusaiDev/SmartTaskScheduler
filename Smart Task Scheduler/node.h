b655fbcdd69e15d8e8aa12292fd89dcc9e0b919e
azbayor2 <hanharry2@gmail.com>
2025-10-07 20:12:01 +0900

    Node.h Node.cpp 추가

--- Smart Task Scheduler/node.h
+++ Smart Task Scheduler/node.h
@@ -5,10 +5,13 @@ namespace scheduler {
 	class node {
 	private:
 		Task* task;
+		node* next;
+		int task_num;
 	public:
 		void setLink(node* next);
 		node* getLink();
 		void setData(Task* data);
 		Task* getData();
+		node(Task * task, int task_num);
 	};
 }
