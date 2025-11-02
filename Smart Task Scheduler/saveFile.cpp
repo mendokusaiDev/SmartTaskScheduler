b04d79f275629277fc2a6600c68ddbdc985d943d
azbayor2 <hanharry2@gmail.com>
2025-11-02 21:22:12 +0900

    수정  - saveFile class 추가

---
+++ Smart Task Scheduler/saveFile.cpp
@@ -0,0 +1,16 @@
+#include "saveFile.h"
+#include "Task.h"
+
+namespace scheduler {
+
+
+	saveFile::saveFile(Task* t) {
+		enddate = t->getEndDate();
+		duration = t->getDuration();
+		type = t->getType();
+		starttime = t->getstarttime();
+		endtime = t->getendtime();
+		TaskNum = t->getTaskNum();
+		finished = t->isfinished();
+	}
+}
