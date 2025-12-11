64195c67370017d70eeb6287823bf4b6a873859b
dwgllml <rjswjsgka@gmail.com>
2025-12-11 22:07:44 +0900

    TUI
    feat(TUI): 메인 메뉴 달력 뷰 및 고정/차단 시간 입력 추가
    chore(TUI): 고정 일정/작업 불가 시간대 함수 선언 추가
    메인 브랜치 커밋

--- Smart Task Scheduler/TUI.h
+++ Smart Task Scheduler/TUI.h
@@ -1,38 +1,44 @@
-﻿#pragma once
+#pragma once
 #include "Calender.h"
 #include <iostream>
 
 namespace scheduler {
 
-	class TUI {
+    class TUI {
 
-	private:
-		Calender* c;
+    private:
+        Calender* c;
 
-		void showMonth();
-		void showWeek();
-		void showDay();
+        void showMonth(int year, int month);
+        void showWeek(int year, int month, int day);
+        void showDay(int year, int month, int day);
 
-		void addTask();
-		void editTask();
-		void deleteTask();
+        void addTask();
+        void editTask();
+        void deleteTask();
 
-		void showStatistics();
+        void showStatistics();
+        void changeInterval();
 
-		void changeInterval();
+        // 새로 추가된 기능
+        void addFixedTask();          // 고정 일정 입력
+        void setUninterruptedTime();  // 작업 불가능 시간대 입력
 
-	public:
-		void mainMenu();
-		TUI() {
-			c = new Calender();
-		};
-		void atExit() {
-			c->save();
-			std::cout << "saved!" << std::endl;
-		}
+        void get_current_time(int& year, int& month, int& day);
+        void get_next_day(int& year, int& month, int& day, int offset);
+        void get_next_week(int& year, int& month, int& day, int offset);
+        void get_next_month(int& year, int& month, int& day, int offset);
+        void get_first_day_of_week(int& year, int& month, int& day);
 
-	};
-
-
+    public:
+        void mainMenu();
+        TUI() {
+            c = new Calender();
+        };
+        void atExit() {
+            c->save();
+            std::cout << "saved!" << std::endl;
+        }
+    };
 
 }
